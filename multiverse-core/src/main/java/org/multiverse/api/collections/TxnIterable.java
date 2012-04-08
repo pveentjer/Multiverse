1a31b3dcfeb1ed64af59afaa0b5175bc56718760
Peter Veentjer <alarmnummer@gmail.com>
2012-04-08 09:54:05 +0300

    TxnIterable javadoc fixes

--- multiverse-core/src/main/java/org/multiverse/api/collections/TxnIterable.java
+++ multiverse-core/src/main/java/org/multiverse/api/collections/TxnIterable.java
@@ -3,6 +3,13 @@ package org.multiverse.api.collections;
 import org.multiverse.api.Txn;
 
 /**
+ * A transactional version of the {@link Iterable}.
+ * <p/>
+ * For every method the Iterable has, an additional method with a transaction is added. Also the
+ * return type is made stronger from Iterator to TxnIterator.
+ * <p/>
+ * If a method is called, without an explicit transaction being passed, it is retrieved
+ * from the {@link org.multiverse.api.TxnThreadLocal}.
  *
  * @param <E>
  * @author Peter Veentjer.
@@ -14,9 +21,29 @@ public interface TxnIterable<E> extends Iterable<E> {
      *
      * @param txn the Txn used for this Operation.
      * @return an Iterator.
+     * @throws NullPointerException if txn is null.
+     * @throws org.multiverse.api.exceptions.ControlFlowError
+     *          if the STM needs to break out from the current
+     *          execution, e.g. to retry a transaction because there was a read conflict. This error should not be
+     *          caught because it will be caught by the {@link org.multiverse.api.TxnExecutor}.
+     * @throws org.multiverse.api.exceptions.TxnExecutionException
+     *          if this operation failed to be executed due
+     *          to a programming error.
      */
     TxnIterator<E> iterator(Txn txn);
 
+    /**
+     * Returns an iterator over a set of elements of type T.
+     *
+     * @return an Iterator.
+     * @throws org.multiverse.api.exceptions.ControlFlowError
+     *          if the STM needs to break out from the current
+     *          execution, e.g. to retry a transaction because there was a read conflict. This error should not be
+     *          caught because it will be caught by the {@link org.multiverse.api.TxnExecutor}.
+     * @throws org.multiverse.api.exceptions.TxnExecutionException
+     *          if this operation failed to be executed due
+     *          to a programming error.
+     */
     @Override
     TxnIterator<E> iterator();
 }
